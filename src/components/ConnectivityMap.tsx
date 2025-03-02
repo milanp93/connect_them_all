c9864dcf241888cfc2db45f3053fb01667d6f378
Milan Popovic <milan.popovic.93@gmail.com>
2025-03-02 16:55:42 +0100

    cleanup

--- src/components/ConnectivityMap.tsx
+++ src/components/ConnectivityMap.tsx
@@ -1,79 +1,78 @@
 'use client'
-import React, { useState, useRef, FC, useEffect } from "react";
-import {
-  GoogleMap,
-  LoadScript,
-  Marker,
-  Polyline,
-  InfoWindow
-} from "@react-google-maps/api";
-import type { GoogleMap as GoogleMapType } from "@react-google-maps/api";
+import React, {  FC } from "react";
+// import {
+//   GoogleMap,
+//   LoadScript,
+//   Marker,
+//   Polyline,
+//   InfoWindow
+// } from "@react-google-maps/api";
+// import type { GoogleMap as GoogleMapType } from "@react-google-maps/api";
 import { PopulationData } from "@/scripts/mergePopulationDenciryCsv";
 import { RecommendationType } from "@/scripts/mergeRecommendationCsv";
 
-const containerStyle = {
-  width: "100%",
-  height: "100vh"
-};
+// const containerStyle = {
+//   width: "100%",
+//   height: "100vh"
+// };
 
-const defaultCenter = { lat: 43.85, lng: 18.40 };
+// const defaultCenter = { lat: 43.85, lng: 18.40 };
 
-const ConnectivityMap: FC<{schoolsAndTowers:PopulationData[], recommended: RecommendationType[]}> = ({schoolsAndTowers, recommended}) => {
-  const [csvData, setCsvData] = useState<PopulationData[]>(schoolsAndTowers);
+const ConnectivityMap: FC<{schoolsAndTowers:PopulationData[], recommended: RecommendationType[]}> = ({ recommended}) => {
+  // const [csvData, setCsvData] = useState<PopulationData[]>(schoolsAndTowers);
 //   const [topSchools, setTopSchools] = useState<SchoolData[]>([]);
-  const [selectedSchool, setSelectedSchool] = useState<PopulationData | null>(null);
-  const [selectedTower, setSelectedTower] = useState<PopulationData | null>(null);
-  const [panPosition, setPanPosition] = useState<{ lat: number; lng: number }>(
-    defaultCenter
-  );
-  const mapRef = useRef<GoogleMapType | null>(null);
-  const [schoolIcon, setSchoolIcon] = useState<any>(null);
-  const [towerIcon, setTowerIcon] = useState<any>(null);
+  // const [selectedSchool, setSelectedSchool] = useState<PopulationData | null>(null);
+  // const [selectedTower, setSelectedTower] = useState<PopulationData | null>(null);
+  // const [panPosition, setPanPosition] = useState<{ lat: number; lng: number }>(
+  //   defaultCenter
+  // );
+  // const mapRef = useRef<GoogleMapType | null>(null);
+  // const [schoolIcon, setSchoolIcon] = useState<any>(null);
+  // const [towerIcon, setTowerIcon] = useState<any>(null);
 
-  const onMapLoad = (map: any) => {
-    mapRef.current = map;
-  };
+  // const onMapLoad = (map: any) => {
+  //   mapRef.current = map;
+  // };
 
-  const panToLocation = (lat: number, lng: number) => {
-    if (mapRef.current) {
-      mapRef.current.panTo({ lat, lng });
-    //   mapRef.current.setZoom(15);
-    }
-  };
+  // const panToLocation = (lat: number, lng: number) => {
+  //   if (mapRef.current) {
+  //     mapRef.current.panTo({ lat, lng });
+  //   //   mapRef.current.setZoom(15);
+  //   }
+  // };
 
-  const handleMarkerClick = (row: PopulationData, type: "school" | "tower") => {
-    if (type === "school") {
-      setSelectedSchool(row);
-      setSelectedTower(null);
-    } else if (type === "tower") {
-      setSelectedTower(row);
-      setSelectedSchool(null);
-    }
-  };
+  // const handleMarkerClick = (row: PopulationData, type: "school" | "tower") => {
+  //   if (type === "school") {
+  //     setSelectedSchool(row);
+  //     setSelectedTower(null);
+  //   } else if (type === "tower") {
+  //     setSelectedTower(row);
+  //     setSelectedSchool(null);
+  //   }
+  // };
 
-  const handlePolylineClick = (distance?: string) => {
-    if (distance) {
-      alert(`Distance: ${distance} km`);
-    } else {
-      alert("Distance not available");
-    }
-  };
+  // const handlePolylineClick = (distance?: string) => {
+  //   if (distance) {
+  //     alert(`Distance: ${distance} km`);
+  //   } else {
+  //     alert("Distance not available");
+  //   }
+  // };
 
   // Define custom marker icons if you have them (ensure these images exist in /public)
 
-    useEffect(() => {
-        setSchoolIcon(window?.google?.maps ? {
-            url: `${process.env.PUBLIC_URL}/school-icon.png`,
-            scaledSize: new window.google.maps.Size(30, 30)
-        } : null);
+    // useEffect(() => {
+    //     setSchoolIcon(window?.google?.maps ? {
+    //         url: `${process.env.PUBLIC_URL}/school-icon.png`,
+    //         scaledSize: new window.google.maps.Size(30, 30)
+    //     } : null);
 
-        setTowerIcon(window?.google?.maps ? {
-            url: "/signal-tower.png",
-            scaledSize: new window.google.maps.Size(30, 30)
-        } : null);
-    }, []);
+    //     setTowerIcon(window?.google?.maps ? {
+    //         url: "/signal-tower.png",
+    //         scaledSize: new window.google.maps.Size(30, 30)
+    //     } : null);
+    // }, []);
 
-    console.log("process.env.GOOGLE_ELEVATION_API_KEY: ",process.env.GOOGLE_ELEVATION_API_KEY)
   return (
     <div style={{ display: "flex", height: "100vh" }}>
       {/* Sidebar with Top 5 Schools */}
